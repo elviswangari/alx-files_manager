39f6f4ebe550ed86664dc10926716b3a6ec51d0f
elviswangari <elviswangare@gmail.com>
2023-09-05 18:51:57 +0300

    caching using redis

---
+++ README.md
@@ -0,0 +1,39 @@
+# FILE MANAGERS
+
+<hr>
+## project summary
+
+the objectives of the project include
+	
+	upload and view files:
+		user authentication via token
+		list all files
+		upload a new file
+		change file permission of a file
+		view a file
+		generate a thumbnail of an image
+	
+the technologies to be used includes
+	Node JS -> exec of JS
+	Express JS -> JS framework
+	Mongo DB ->  NoSQL DB
+	Redis -> cache
+	Kue ->
+	Babel -> convert es6 to es5
+	
+some npm dependancies include:
+	mocha
+	Nodemon -> reload server on save
+	image-thumbnail
+	mime-types -> to utilize mime types
+	Bull
+	chai and chai-http
+	eslint
+	request
+	sinon
+
+
+<hr>
+## Author
+
+Elvis Wangari: [Linkedin](https://linkedin.com/in/elvis-wangari)
---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor () {
+    this.client = createClient();
+    this.client.on('error', err => console.log(err));
+    this.connected = false;
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+}
+  isAlive() {
+    // returns true when redis con is ok else false
+    return this.connected;
+  }
+
+  async get(key) {
+    // return redis value stored for the key
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key,value, duration) {
+    // store key value in redis in duration given
+    return new Promise((resolve, reject) => {
+    this.client.setex(key, duration, value, (error) => {
+      if (error) {
+        reject(error);
+      }
+    });
+    });
+
+  }
+
+  async del(key) {
+    // remove value from redis 
+    return new Promise((resolve, reject) => {
+    this.client.del(key, (error) => {
+        if(error){
+          reject(error);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
