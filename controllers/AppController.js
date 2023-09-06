f4048799e11da41064f008419f2d7f21d5a0354a
elviswangari <elviswangare@gmail.com>
2023-09-06 23:21:15 +0300

    routes, controllers

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,22 @@
-const utilsRedis = require('../utils/redis');
-const utilsDB = require('../utils/db');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
 class AppController {
   static getStatus(req, res) {
-    const redis = utilsRedis.redisClient.isAlive();
-    const db = utilsDB.dbClient.isAlive();
+    const redis = redisClient.isAlive();
+    const db = dbClient.isAlive();
 
-    return res.status(200).send({
+    res.status(200).json({
       redis,
       db,
     });
   }
 
   static getStats(req, res) {
-    const users = utilsDB.dbClient.nbUsers();
-    const files = utilsDB.dbClient.nbFiles();
+    const users = dbClient.nbUsers();
+    const files = dbClient.nbFiles();
 
-    return res.status(200).send({
+    res.status(200).json({
       users,
       files,
     });
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json);
 app.use(routes);
 
 app.listen(port, () => {
