b5099f35a24263673fc18f650fb73bf81ee09421
elvis wangari <elviswangare@gmail.com>
2023-09-10 15:01:10 +0300

    AppController

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,24 +1,38 @@
 import { redisClient } from '../utils/redis';
 import { dbClient } from '../utils/db';
 
-exports.getStatus = (req, res) => {
-  const redisStatus = redisClient.isAlive();
-  const dbStatus = dbClient.isAlive();
-  if (redisStatus && dbStatus) {
-    res.send({
-      redis: redisStatus,
-      db: dbStatus,
-    });
+exports.getStatus = async (req, res) => {
+  try {
+    const redisStatus = await redisClient.isAlive();
+    const dbStatus = await dbClient.isAlive();
+
+    if (redisStatus && dbStatus) {
+      res.status(200).json({
+        redis: redisStatus,
+        db: dbStatus,
+      });
+      return;
+    }
+
+    // If any of the conditions are not met, it falls through to the error handling
+  } catch (error) {
+    console.error('Error checking status:', error);
   }
 };
 
-exports.getStats = (req, res) => {
-  const usersStats = dbClient.nbUsers;
-  const filesStats = dbClient.nbFiles;
-  if (usersStats && filesStats) {
-    res.send({
-      users: usersStats,
-      files: filesStats,
-    });
+exports.getStats = async (req, res) => {
+  try {
+    const usersStats = await dbClient.nbUsers();
+    const filesStats = await dbClient.nbFiles();
+
+    if (usersStats !== -1 && filesStats !== -1) {
+      res.status(200).json({
+        users: usersStats,
+        files: filesStats,
+      });
+      return;
+    }
+  } catch (error) {
+    console.error('Error getting stats from db:', error);
   }
 };
